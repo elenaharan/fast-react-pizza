36ee4d49222fd10e1a3e3957f6c86ca9a886417c
Elena <elenaharan@gmail.com>
2024-06-03 07:42:50 +0100

    Building the cart page

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,36 +1,17 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
-import CartItem from './CartItem'
-import { useSelector } from 'react-redux';
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import CartItem from './CartItem';
+import EmptyCart from './EmptyCart';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
+import {getUsername } from '../user/userSlice';
 
 function Cart() {
-  const username = useSelector(store => store.user.username);
+  const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
 
-  const cart = fakeCart;
+  if (!cart.length) return <EmptyCart />
 
   return (
     <div className='px-4 py-3'>
@@ -44,7 +25,7 @@ function Cart() {
 
       <div className='mt-6 space-x-2'>
         <Button to="/order/new" type="primary">Order pizzas</Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>Clear cart</Button>
       </div>
     </div>
   );
