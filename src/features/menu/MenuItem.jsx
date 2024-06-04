1e63bbd601a8c109d3e4a350a6c7623dd067c1f3
Elena <elenaharan@gmail.com>
2024-06-04 07:29:57 +0100

    Update item quantity

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import Button from '../../ui/Button'
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteButton from '../cart/DeleteButton';
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 
 function MenuItem({ pizza }) {
@@ -35,7 +36,11 @@ function MenuItem({ pizza }) {
         <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(", ")}</p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p  className='text-sm font-medium uppercase text-stone-500'>Sold out</p>}
-          {isInCart && <DeleteButton pizzaId={id} />}
+          {isInCart && <div className="flex items-center gap-3 sm:gap-8">
+            <UpdateItemQuantity pizzaId={id} currentQuantity={currentQuantity}/>
+            <DeleteButton pizzaId={id} />
+            </div>
+            }
           {!soldOut && !isInCart && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
         </div>
       </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,8 +6,9 @@ function Button({children, disabled, to, type, onClick}) {
 	const base = "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed text-sm";
 
 const styles = {
+	round: base + ' px-2.5 py-1 md:px-3.5 md:py-2.5 text-xs',
 	primary: base + ' py-3 px-4 md:px-6 md:py-4',
-	small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+	small: base + ' px-4 py-2 md:px-5 md:py-2 text-sm',
 	secondary: "py-2.4 px-4 md:px-6 md:py-3.5 border-2 border-stone-300uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed text-sm"
 }
 
